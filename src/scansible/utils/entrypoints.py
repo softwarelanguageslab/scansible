91b58626b0f50e23c60d692f6f24525b6737c366
Ruben Opdebeeck <ropdebee@soft.vub.ac.be>
2022-12-11 11:33:58 +0100

    fix(utils): ignore symlinks in entrypoints, silence Ansible warnings

--- src/scansible/utils/entrypoints.py
+++ src/scansible/utils/entrypoints.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 
 from typing import Literal
 
+import os
 from collections.abc import Iterable
+from contextlib import redirect_stderr
 from pathlib import Path
 
 from ansible.errors import AnsibleError
@@ -50,7 +52,8 @@ def is_entrypoint(path: Path) -> bool:
 
     # Finally, parse the YAML and check in more detail
     try:
-        yaml_obj = DataLoader().load(content)
+        with open(os.devnull, 'w') as devnull, redirect_stderr(devnull):
+            yaml_obj = DataLoader().load(content)
     except AnsibleError:
         # If the file fails to parse, don't consider it an entrypoint.
         return False
@@ -66,6 +69,9 @@ def find_entrypoints(root: Path) -> list[tuple[Path, Literal['role', 'playbook']
     return list(_find_entrypoints(root.resolve()))
 
 def _find_entrypoints(root: Path) -> Iterable[tuple[Path, Literal['role', 'playbook']]]:
+    if root.is_symlink():
+        return
+
     # Check current path.
     if is_entrypoint(root):
         yield root, 'playbook' if root.is_file() else 'role'
@@ -73,6 +79,7 @@ def _find_entrypoints(root: Path) -> Iterable[tuple[Path, Literal['role', 'playb
     if root.is_file() or root.name.startswith('.') or 'test' in root.name.lower() or 'molecule' in root.name.lower():
         return
 
-    for child in root.iterdir():
-        yield from _find_entrypoints(child)
+    if root.is_dir():
+        for child in root.iterdir():
+            yield from _find_entrypoints(child)
 
