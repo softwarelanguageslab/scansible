20e0a94a12a985e63b0aa811699ed0698a0774b3
Ruben Opdebeeck <ropdebee@soft.vub.ac.be>
2022-12-12 18:55:26 +0100

    fix(checks): don't report lines twice
    Smells can be reached through multiple paths, but since we only
    report the path of the source and exclude the sink path, we should
    deduplicate it properly.

--- src/scansible/checks/security/__init__.py
+++ src/scansible/checks/security/__init__.py
@@ -11,7 +11,7 @@ from .rules import get_all_rules, RuleResult
 
 def run_all_checks(pdg: Graph) -> list[tuple[str, str]]:
     rules = get_all_rules()
-    results = []
+    results = set()
     pdg_import_query = neo4j_dump(pdg)
 
     if not pdg_import_query.strip():
@@ -27,21 +27,21 @@ def run_all_checks(pdg: Graph) -> list[tuple[str, str]]:
 
         for rule in rules:
             raw_results = rule.run(graph)
-            results.extend(_convert_results(raw_results))
+            results |= _convert_results(raw_results)
         logger.info(f'Ran queries in {(datetime.now() - prev_time).total_seconds():.2f}s')
         prev_time = datetime.now()
 
     logger.info(f'Cleaned up in {(datetime.now() - prev_time).total_seconds():.2f}s')
     logger.info(f'Running checks took a total of {(datetime.now() - start_time).total_seconds():.2f}s')
 
-    return results
+    return sorted(results, key=lambda res: res[1])
 
 
-def _convert_results(results: list[RuleResult]) -> list[tuple[str, str]]:
-    new_results = []
+def _convert_results(results: list[RuleResult]) -> set[tuple[str, str]]:
+    new_results = set()
     for name, source_loc, sink_loc, _ in results:
         if source_loc == 'unknown file:-1:-1':
             source_loc = sink_loc
-        new_results.append((name, source_loc))
+        new_results.add((name, source_loc))
 
     return new_results
